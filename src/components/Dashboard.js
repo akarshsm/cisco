d40f8ee1153ac7080ef2b417728f12a0b75e7aac
akarshsm <akarshsm@gmail.com>
2018-12-11 19:41:20 +0530

    added total price and checkout button

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 import Product from './Product';
+import {connect} from 'react-redux';
+
 
 import './Dashboard.css';
 
 class Dashboard extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            // cart_value: this.props.product
+        };
+    }
+
+    renderCartDetails(){
+        if(this.props.cart_products.length>0){
+            var cart_value =0;
+            this.props.cart_products.forEach(function(product) {
+                cart_value += parseInt(product.product_price);
+            });
+            return(
+                <div className="cart-details d-flex flex-row mt-2">
+                    <div className="ml-auto d-flex align-items-center mr-2">
+                        <span className="font-weight-bold">Total Price :</span>
+                        <span className="font-weight-bold">{cart_value}</span>
+                    </div>
+                    <div className="mr-3">
+                        <a className="btn btn-danger" href="/checkout">Checkout</a>
+                    </div>
+
+                </div>
+            );
+        }   
+    }
     
     renderProducts(){
         return this.props.products.map(product => {
@@ -27,11 +57,18 @@ class Dashboard extends Component{
 
     render(){
         return(
-            <div className="container-fluid d-flex flex-row product-container">
-                {this.renderProducts()}
+            <div className="dashboard">
+               {this.renderCartDetails()}
+
+                <div className="container-fluid d-flex flex-row product-container">
+                    {this.renderProducts()}
+                </div>
             </div>
         );
     }
 }
+function mapStateToProps(state) {
+    return { cart_products : state.cart_products};
+}
 
-export default Dashboard;
+export default connect(mapStateToProps)(Dashboard);
